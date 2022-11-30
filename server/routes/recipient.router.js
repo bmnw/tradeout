f9c8fadb8a11c4e78e8f8739b4e90695845b58e6
Jack Steiner <steiner.jack01@gmail.com>
2022-11-29 20:27:45 -0600

    started sendgrid mail for recipient router

--- server/routes/recipient.router.js
+++ server/routes/recipient.router.js
@@ -16,4 +16,28 @@ router.get('/:id', (req, res) => {
     });
 });
 
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+router.get('/api/recipient', async (req, res) => {
+    const secondPartyEmail = req.params.newContractDetails.secondPartyEmail
+    const msg = {
+    to: secondPartyEmail, // Change to your recipient
+    from: process.env.SENDGRID_EMAIL, // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    
+    sgMail
+    .send(msg)
+    .then((response) => {
+        console.log(response[0].statusCode)
+        console.log(response[0].headers)
+        res.send(200);
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+})
+
 module.exports = router;
